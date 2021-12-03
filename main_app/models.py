2c5f7ccfe204baefd2f6489b36a22995771a0592
missmercy0310 <mistrexxmercy@gmail.com>
2021-12-02 16:59:02 -0800

    Styling begins and fixes user detail issue

--- main_app/models.py
+++ main_app/models.py
@@ -12,7 +12,7 @@ class RPG(models.Model):
     classes_name = models.CharField(max_length=500, default="Classes")
     entities_name = models.CharField(max_length=500, default="Playable Entities")
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="rpgs")
 
     def __str__(self):
         return self.title
