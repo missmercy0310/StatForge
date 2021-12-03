cf04c23f0446f7af178cf74e01700025c95018f6
missmercy0310 <mistrexxmercy@gmail.com>
2021-12-02 23:12:43 -0800

    Config deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn stat_forge_project.wsgi
--- stat_forge_project/settings.py
+++ stat_forge_project/settings.py
@@ -141,3 +141,6 @@ SASS_PROCESSOR_ROOT = STATIC_ROOT
 LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = '/'
+
+import django_heroku
+django_heroku.settings(locals())
